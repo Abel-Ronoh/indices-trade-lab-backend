0386d079c872fb1a91aaf84a61e429fc7c29b00b
“Einstein1998” <danieleinstein1998@mail.com>
2023-07-24 10:43:15 +0300

    Addition f backend functionality
    Backend Functionality: Define the backend functionality in the form of small, stateless functions. Each function should handle a specific task, such as user authentication, processing trades, or fetching market data.

---
+++ BackendFunctionality.py
@@ -0,0 +1,64 @@
+#User Authentication:
+
+#For user authentication,use a library like Flask along with Flask-JWT-Extended for JSON Web Token (JWT) based authentication. 
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+jwt = JWTManager(app)
+
+# Dummy user database( replace with appropriate database)
+users = {
+    'john': {'password': 'password123'},
+    'jane': {'password': 'mypassword'},
+}
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username in users:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    users[username] = {'password': password}
+    return jsonify({'message': 'User registered successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username not in users or users[username]['password'] != password:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify({'access_token': access_token}), 200
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected_route():
+    current_user = get_jwt_identity()
+    return jsonify({'message': f'Hello, {current_user}! You are authenticated.'}), 200
+
+if __name__ == '__main__':
+    app.run()
+# This code snippet sets up a basic Flask app with user registration, login, and a protected route that requires a valid JWT token for access.
+
+#Fetching Market Data:
+#Fetching market data usually involves making API calls to external data providers.
+import requests
+
+@app.route('/market_data', methods=['GET'])
+def fetch_market_data():
+    currency_pair = request.args.get('currency_pair')  # Example: 'EURUSD'
+    # Make an API call to get market data for the currency pair
+    response = requests.get(f'https://api.example.com/market_data/{currency_pair}')
+    if response.status_code == 200:
+        market_data = response.json()
+        return jsonify(market_data), 200
+    else:
+        return jsonify({'message': 'Failed to fetch market data'}), 500
