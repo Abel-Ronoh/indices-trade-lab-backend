05addaffaba6d4f180bfc6c7d5272bd4f1fd36ea
“Einstein1998” <danieleinstein1998@mail.com>
2023-07-25 19:38:40 +0300

    Update #Mpesa-Get Away.py

--- #Mpesa-Get Away.py
+++ #Mpesa-Get Away.py
@@ -1,12 +1,13 @@
 #Mpesa-Get Away
 #Request
+
 Headers
 Key: Authorization
 Value: Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==
 ​
 Body
   {
-    "ShortCode": 600992,
+    "ShortCode": 600999,
     "ResponseType": "Completed",
     "ConfirmationURL": "https://mydomain.com/confirmation",
     "ValidationURL": "https://mydomain.com/validation"
